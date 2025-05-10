8fa924709665e4c57253a89642521868a68d2832
Gogo200202 <gosheto420@gmail.com>
2025-05-10 12:33:34 +0300

    add auto-update

---
+++ index.js
@@ -0,0 +1,125 @@
+const axios = require("axios");
+
+const fs = require("fs");
+
+const md = require("markdown-it")({
+  html: true, // Enable HTML tags in source
+  breaks: false, // Convert '\n' in paragraphs into <br>
+});
+
+const TIMEZONE_OFFSET = 7;
+
+const QUOTES_API = "https://zenquotes.io/api/quotes";
+
+(async () => {
+  const { today, hour } = getCurrentTime();
+  const greetings = generateGreetings(hour);
+  const { quote, author } = await getQuotes(hour);
+
+  const text = `### ${greetings}
+
+  ![Alt Text](https://media1.tenor.com/m/pvFJwncehzIAAAAC/hello-there-private-from-penguins-of-madagascar.gif)
+  
+  # 💫 About Me:
+  🔭 I’m currently working on My very little 3d renderer<br>👯 I’m looking to collaborate on open sores projects<br>🤝 I’m looking for help with open sores projects<br>💬 Ask me about anything <br>⚡ I like challenging myself
+  
+  
+  ## 🌐 Socials:
+  [![LinkedIn](https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white)](https://linkedin.com/in/georgi-angelov-42b720255) 
+  [![LeetCode](https://img.shields.io/badge/LeetCode-000000?style=for-the-badge&logo=LeetCode&logoColor=#d16c06)](https://leetcode.com/u/Gogo200202/)
+  
+  # 💻 Tech Stack:
+  ![C#](https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white) ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) ![Java](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white) ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E) ![Markdown](https://img.shields.io/badge/markdown-%23000000.svg?style=for-the-badge&logo=markdown&logoColor=white) ![TypeScript](https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white) ![.Net](https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white) ![Blazor](https://img.shields.io/badge/blazor-%235C2D91.svg?style=for-the-badge&logo=blazor&logoColor=white) ![Bootstrap](https://img.shields.io/badge/bootstrap-%238511FA.svg?style=for-the-badge&logo=bootstrap&logoColor=white) ![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB) ![JWT](https://img.shields.io/badge/JWT-black?style=for-the-badge&logo=JSON%20web%20tokens) ![NPM](https://img.shields.io/badge/NPM-%23CB3837.svg?style=for-the-badge&logo=npm&logoColor=white) ![Next JS](https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white) ![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white) ![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB) ![React Hook Form](https://img.shields.io/badge/React%20Hook%20Form-%23EC5990.svg?style=for-the-badge&logo=reacthookform&logoColor=white) ![React Router](https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white) ![Socket.io](https://img.shields.io/badge/Socket.io-black?style=for-the-badge&logo=socket.io&badgeColor=010101) ![Spring](https://img.shields.io/badge/spring-%236DB33F.svg?style=for-the-badge&logo=spring&logoColor=white) ![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white) ![Three js](https://img.shields.io/badge/threejs-black?style=for-the-badge&logo=three.js&logoColor=white) ![Vite](https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white) ![MicrosoftSQLServer](https://img.shields.io/badge/Microsoft%20SQL%20Server-CC2927?style=for-the-badge&logo=microsoft%20sql%20server&logoColor=white) ![MongoDB](https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white) ![Postgres](https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white) ![Figma](https://img.shields.io/badge/figma-%23F24E1E.svg?style=for-the-badge&logo=figma&logoColor=white) ![Docker](https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white) ![GitHub Actions](https://img.shields.io/badge/github%20actions-%232671E5.svg?style=for-the-badge&logo=githubactions&logoColor=white) ![Git](https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white) ![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white) ![Postman](https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&logo=postman&logoColor=white)
+  # 📊 GitHub Stats:
+  ![](https://github-readme-stats.vercel.app/api?username=Gogo200202&theme=dark&hide_border=false&include_all_commits=true&count_private=false)<br/>
+  ![](https://github-readme-streak-stats.herokuapp.com/?user=Gogo200202&theme=dark&hide_border=false)<br/>
+  ![](https://github-readme-stats.vercel.app/api/top-langs/?username=Gogo200202&theme=dark&hide_border=false&include_all_commits=true&count_private=false&layout=compact)
+  
+  ## 🏆 GitHub Trophies
+  ![](https://github-profile-trophy.vercel.app/?username=Gogo200202&theme=dark&no-frame=false&no-bg=true&margin-w=4)
+  
+  ### 🔝 Top Contributed Repo
+  ![](https://github-contributor-stats.vercel.app/api?username=Gogo200202&limit=5&theme=dark&combine_all_yearly_contributions=true)
+  
+  ---
+  [![](https://visitcount.itsvg.in/api?id=Gogo200202&icon=1&color=12)](https://visitcount.itsvg.in)
+  
+  <!-- Proudly created with GPRM ( https://gprm.itsvg.in ) -->
+  
+  ## Quote of the day:
+  *"${quote}"* <br>
+  — ${author}
+`;
+
+  const content = md.renderInline(text);
+  generateFile(content);
+
+  /* Timestamp */
+  console.log(`⏳ Running at ${today} UTC +0${TIMEZONE_OFFSET}:00`);
+})();
+
+function getCurrentTime() {
+  const today = new Date();
+  today.setHours(today.getHours() + TIMEZONE_OFFSET);
+  const hour = today.getHours();
+  const minute = today.getMinutes();
+  // check if the hour >= 24
+  if (hour >= 24) {
+    return Math.abs(24 - hour);
+  }
+  return {
+    today,
+    hour,
+    minute
+  };
+}
+
+function isWeekend(date = getCurrentTime().today) {
+  return date.getDay() === 6 || date.getDay() === 0;
+}
+
+function generateGreetings(time) {
+  const goodMorning = "Good morning ☀️";
+  const goodAfternoon = "Good afternoon 👋";
+  const goodEvening = "Good evening ☕";
+  const goodNight = "Good night 😴";
+  const happyWeekend = "Happy weekend 🏝️";
+
+  if (isWeekend()) {
+    return happyWeekend;
+  }
+  if (time >= 4 && time < 11) {
+    return goodMorning;
+  }
+  if (time >= 11 && time < 16) {
+    return goodAfternoon;
+  }
+  if (time >= 16 && time < 23) {
+    return goodEvening;
+  }
+  return goodNight;
+}
+
+async function getQuotes(time) {
+    return await axios.get(QUOTES_API).then((response) => {
+        if (response.data.length === 0) {
+          return {
+            quote: "There is no result without struggle, there is no struggle without sacrifice.",
+            author: "Me",
+          };
+        }
+        const { q, a } = response.data[0];
+        return {
+          quote: q,
+          author: a,
+        };
+      });
+}
+
+function generateFile(contents) {
+  const targetFile = "README.md";
+  fs.writeFile(targetFile, contents, function (err) {
+    if (err) return console.log(`⛔ [FAILED]: ${err}`);
+    console.log("✅ [SUCCESS]: README.md has been generated.");
+  });
+}
